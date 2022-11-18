7661638dd2c56ff9241ee4559a1ec58bdfc6b806
alexisnava-stori <nasabmx@hotmail.com>
2022-11-18 10:23:21 -0600

    First look at composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Reactive vs Ref</h1>
+    <h3>{{ ironman }}</h3>
+    <h3>{{ hulk }}</h3>
+
+    <button @click="changeIronman">Cambiar Ironman</button>
+    <button @click="changeHulk">Cambiar Hulk</button>
+
+    <h3>Ironman: {{ ironmanIsAlive }}</h3>
+    <h3>Hulk: {{ hulkIsAlive }}</h3>
+    <button @click="changeStatus">Cambiar Status</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { ref, reactive } from 'vue';
 
 export default {
   name: 'HomeView',
+
+  setup() {
+    const ironman = ref({ name: 'Toni', age: 50 })
+    const hulk = reactive({ name: 'Bruce', age: 50 })
+
+    let ironmanIsAlive = reactive(false)
+    const hulkIsAlive = ref(true)
+
+    const changeStatus = () => {
+      hulkIsAlive.value = !hulkIsAlive.value
+      ironmanIsAlive = reactive(!ironmanIsAlive)
+    }
+
+    return {
+      ironman,
+      hulk,
+
+      ironmanIsAlive,
+      hulkIsAlive,
+
+      changeIronman: () => {
+        ironman.value.name = 'Stark'
+        ironman.value.age = 24
+      },
+      changeHulk: () => {
+        hulk.name = 'Banner'
+        hulk.age = 27
+      },
+
+      changeStatus
+    }
+  }
 }
 </script>
